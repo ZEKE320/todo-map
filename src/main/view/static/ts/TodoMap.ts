29a7e8257cbeeba96c9eabdacbe6c07cd93d9cc5
ZEKE320 <46393375+ZEKE320@users.noreply.github.com>
2023-12-02 01:23:18 +0900

    ⚡️ 不要なサービスワーカー関連の処理を削除

--- src/main/view/static/ts/TodoMap.ts
+++ src/main/view/static/ts/TodoMap.ts
@@ -16,19 +16,6 @@ import {
 
 import "sass/todo-map.scss";
 
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("/service-worker.js")
-      .then((registration) => {
-        console.log("SW registered: ", registration);
-      })
-      .catch((registrationError) => {
-        console.log("SW registration failed: ", registrationError);
-      });
-  });
-}
-
 /** 目標達成済みならばtrue */
 let achievedGoal = false;
 
