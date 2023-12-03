9432e17dc6cf0dbbe1ee3e8dd8c2f1db45a38096
ZEKE320 <46393375+ZEKE320@users.noreply.github.com>
2023-12-03 22:50:14 +0900

    ✨ サイドメニューの実装を追加

---
+++ src/lib/components/common/SideMenu.tsx
@@ -0,0 +1,50 @@
+import Box from "@mui/material/Box";
+import IconButton from "@mui/material/IconButton";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import Menu from "@mui/icons-material/Menu";
+import Drawer from "@mui/material/Drawer";
+import TungstenIcon from "@mui/icons-material/Tungsten";
+import DoneOutlineIcon from "@mui/icons-material/DoneOutline";
+import UnpublishedIcon from "@mui/icons-material/Unpublished";
+import { useState } from "react";
+
+const SideMenu = () => {
+  const [openDrawer, setOpenDrawer] = useState(false);
+
+  return (
+    <Box>
+      <IconButton
+        color="inherit"
+        aria-label="menu"
+        onClick={() => setOpenDrawer(true)}
+      >
+        <Menu />
+      </IconButton>
+      <Drawer
+        anchor="left"
+        open={openDrawer}
+        onClose={() => setOpenDrawer(false)}
+        color="#030c12"
+        elevation={1}
+      >
+        <List>
+          <ListItem onClick={() => setOpenDrawer(false)}>
+            <TungstenIcon sx={{ marginRight: "1em" }} />
+            達成できるTODO
+          </ListItem>
+          <ListItem onClick={() => setOpenDrawer(false)}>
+            <DoneOutlineIcon sx={{ marginRight: "1em" }} />
+            達成したTODO
+          </ListItem>
+          <ListItem onClick={() => setOpenDrawer(false)}>
+            <UnpublishedIcon sx={{ marginRight: "1em" }} />
+            まだ達成できないTODO
+          </ListItem>
+        </List>
+      </Drawer>
+    </Box>
+  );
+};
+
+export default SideMenu;
