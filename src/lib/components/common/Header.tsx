3e2a4253d499a8e9ad3bdd25bed271754540bde7
ZEKE320 <46393375+ZEKE320@users.noreply.github.com>
2023-12-03 22:52:28 +0900

    💄 ヘッダーの表示を調整

--- src/lib/components/common/Header.tsx
+++ src/lib/components/common/Header.tsx
@@ -3,42 +3,45 @@
 import PageReloadingBtn from "@/lib/components/PageReloadingBtn";
 import { darkTheme } from "@/lib/ts/mui/ThemeHandler";
 import { HeaderData } from "@/lib/type";
-import Menu from "@mui/icons-material/Menu";
+import MapIcon from "@mui/icons-material/Map";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
-import IconButton from "@mui/material/IconButton";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import ThemeProvider from "@mui/material/styles/ThemeProvider";
+import SideMenu from "@/lib/components/common/SideMenu";
 
 const Header = ({ title }: HeaderData) => {
   return (
     <ThemeProvider theme={darkTheme}>
       <AppBar elevation={0} position="static">
-        <Toolbar variant="dense">
+        <Toolbar
+          variant="dense"
+          sx={{ display: "flex", justifyContent: "space-between" }}
+        >
           <Box
             sx={{
-              marginRight: "auto",
               display: "flex",
               flexWrap: "nowrap",
               alignItems: "center",
             }}
           >
-            <IconButton color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-              <Menu />
-            </IconButton>
+            <SideMenu />
             <Typography
-              variant="h4"
+              variant="h5"
               fontFamily="inherit"
               fontWeight="bolder"
               color="inherit"
+              display="flex"
+              alignItems="center"
               component="div"
             >
               {title}
+              <MapIcon />
             </Typography>
           </Box>
           <Box>
-            <PageReloadingBtn label="次の目標を設定する"></PageReloadingBtn>
+            <PageReloadingBtn label="次の目標を設定する" />
           </Box>
         </Toolbar>
       </AppBar>
